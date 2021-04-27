065913b74a86bba9fa2dff99d2c36a80c422ec8e
Randy Rodriguez <Randyr2125@msn.com>
2021-04-26 20:30:10 -0400

    cleaned up the instructions for creating a hash

--- store_item.rb
+++ store_item.rb
@@ -1,13 +1,3 @@
-# Exercise:
-# A. Use the terminal to create a new folder in your Actualize folder called practice_ruby.
-# B. Create a new file in the folder called store_item.rb and open it in your text editor.
-# C. Use hashes with symbols to represent the following scenario:
-# C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
-# C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
-# C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
-# C.4 Print out the attributes in a sentence that describes the item
-
-
 # item1 = {:item_name => "Banana", :price => 2.00, :color => "yellow"}
 # item2 = {:item_name => "Orange", :price => 1.50, :color => "orange"}
 # item3 = {:item_name => "Apple", :price => 1.25, :color => "red"}
@@ -88,10 +78,10 @@ table = TTY::Table.new(["First Name","Last name","Salary","Active"],
 
 puts table.render(:ascii)
 p table[0]
-# id_index = []
-# table.each_with_index  { |row, index| 
+id_index = []
+table.each_with_index  { |row, index| 
 
-# id_index << row
+id_index << row
 
-# }
-# p id_index
+}
+p id_index
